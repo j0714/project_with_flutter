72228ec0972a066864f6349976e7ffc31586eb17
pockypepe <thdgurws98@gmail.com>
2022-06-07 11:55:19 +0900

    데이터베이스 접근 시도

--- lib/model/addNotice.dart
+++ lib/model/addNotice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ui_1/color/shareColor.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AddNotice extends StatefulWidget {
   const AddNotice({Key? key}) : super(key: key);
@@ -9,6 +10,18 @@ class AddNotice extends StatefulWidget {
 }
 
 class _AddNoticeState extends State<AddNotice> {
+  final _authentication = FirebaseAuth.instance;
+  // final _formKey = GlobalKey<FormState>();
+  String addTitle = '';
+  String addText = '';
+
+  // void _tryValidation(){
+  //   final isValid = _formKey.currentState!.validate();
+  //   if(isValid){
+  //     _formKey.currentState!.save();
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -57,89 +70,123 @@ class _AddNoticeState extends State<AddNotice> {
         ),
         body: Scaffold(
           backgroundColor: ColorSet.pageBackgroundColor,
-        body : Padding(
-          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: 20,),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Notice title',
-                    hintStyle: TextStyle(fontWeight: FontWeight.bold),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    border: InputBorder.none,
-                    filled: true,
-                    fillColor: Colors.white,
+          body: Padding(
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
-                SizedBox(height: 20,),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Write notice',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(8)),
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    border: InputBorder.none,
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  maxLines: 20,
-                ),
-                SizedBox(height : 20),
-                Row(
-                  // crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        padding: EdgeInsets.only(
-                            left: 8, right: 8, top: 2, bottom: 2),
-                        height: 30,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            // color: ColorSet.appBarColor,
-                            color: ColorSet.appBarColor),
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.add,
-                              color: Colors.white,
-                              size: 20,
-                            ),
-                            SizedBox(
-                              width: 2,
-                            ),
-                            Text(
-                              'Add New',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
+                  TextFormField(
+                    key: ValueKey(1),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please write notice!';
+                      } else
+                        return null;
+                    },
+                    onSaved: (value) {
+                      addTitle = value!;
+                    },
+                    onChanged: (value) {
+                      addTitle = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Notice title',
+                      hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        borderSide: BorderSide(color: Colors.blue),
                       ),
-                      // Row(
-                      //   // crossAxisAlignment: CrossAxisAlignment.end,
-                      //   mainAxisAlignment: MainAxisAlignment.end,
-                      //   children: [
-                      //     IconButton(onPressed: () {}, icon: Icon(Icons.bento_sharp))
-                      //   ],
-                      // )
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.white,
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    key: ValueKey(2),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please write notice!';
+                      } else
+                        return null;
+                    },
+                    onSaved: (value) {
+                      addText = value!;
+                    },
+                    onChanged: (value) {
+                      addText = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Write notice',
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                    maxLines: 20,
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    // crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // _tryValidation();
+                          print(addTitle);
+                          print(addText);
+                        },
+                        child: Container(
+                          padding: EdgeInsets.only(
+                              left: 8, right: 8, top: 2, bottom: 2),
+                          height: 30,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: ColorSet.appBarColor),
+                          child: Row(
+                            children: <Widget>[
+                              Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                              SizedBox(
+                                width: 2,
+                              ),
+                              Text(
+                                'Add Notice',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        // Row(
+                        //   // crossAxisAlignment: CrossAxisAlignment.end,
+                        //   mainAxisAlignment: MainAxisAlignment.end,
+                        //   children: [
+                        //     IconButton(onPressed: () {}, icon: Icon(Icons.bento_sharp))
+                        //   ],
+                        // )
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),),
+        ),
       ),
     );
   }
