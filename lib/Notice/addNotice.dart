f25777e4ce790709b708cba3a5c497bc7b873399
pockypepe <thdgurws98@gmail.com>
2022-06-09 02:16:39 +0900

    testNotice는 나중에 삭제할 것, addNotice는 조건에 따라 스낵바를 출력해 필수 입력사항을 알려주는 기능 추가 & 글 쓸때 뒤로 가기 버튼 누르면 경고창 나오도록 구현 중

--- lib/Notice/addNotice.dart
+++ lib/Notice/addNotice.dart
@@ -14,7 +14,8 @@ class _AddNoticeState extends State<AddNotice> {
   var _addTitle = '';
   var _addText = '';
 
-  void _snackBarMessage() {  //스낵바 활용
+  void _snackBarMessage() {
+    //스낵바 활용
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         content: Expanded(
@@ -22,6 +23,42 @@ class _AddNoticeState extends State<AddNotice> {
         ),
         duration: Duration(
           seconds: 2,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        behavior: SnackBarBehavior.floating, //바닥과 공간 두는 기능
+      ),
+    );
+  }
+
+  void _titleEmptyMessage() {
+    //스낵바 활용
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Expanded(
+          child: Text('Please Write Title!'),
+        ),
+        duration: Duration(
+          seconds: 5,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        behavior: SnackBarBehavior.floating, //바닥과 공간 두는 기능
+      ),
+    );
+  }
+
+  void _textEmptyMessage() {
+    //스낵바 활용
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Expanded(
+          child: Text('Please Write Text!'),
+        ),
+        duration: Duration(
+          seconds: 5,
         ),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
@@ -51,171 +88,153 @@ class _AddNoticeState extends State<AddNotice> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        FocusScope.of(context).unfocus();
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          automaticallyImplyLeading: false,
-          backgroundColor: ColorSet.appBarColor,
-          flexibleSpace: SafeArea(
-            child: Container(
-              padding: EdgeInsets.only(right: 16),
-              child: Row(
-                children: <Widget>[
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            automaticallyImplyLeading: false,
+            backgroundColor: ColorSet.appBarColor,
+            flexibleSpace: SafeArea(
+              child: Container(
+                padding: EdgeInsets.only(right: 16),
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {
+                        if (_addTitle.trim().isEmpty &&
+                            _addText.trim().isEmpty) {
+                          return Navigator.pop(context);
+                        } else
+                        return null; // 얘 떄문에 아무것도 안움직임
+                        // Navigator.pop(context);
+                      },
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'New Notice',
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'New Notice',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            if (_addTitle.trim().isEmpty &&
+                                _addText.trim().isEmpty) {
+                              return null;
+                            } else if (_addText.trim().isEmpty) {
+                              return _textEmptyMessage();
+                            } else if (_addTitle.trim().isEmpty) {
+                              return _titleEmptyMessage();
+                            } else
+                              return _sendNotice();
+                          },
+                          child: Container(
+                            padding: EdgeInsets.only(
+                                left: 8, right: 8, top: 2, bottom: 2),
+                            height: 30,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: Colors.white,
+                            ),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(
+                                  Icons.add,
+                                  color: ColorSet.appBarColor,
+                                  size: 20,
+                                ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Text(
+                                  'Add Notice',
+                                  style: TextStyle(
+                                    color: ColorSet.appBarColor,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        body: Scaffold(
-          backgroundColor: ColorSet.pageBackgroundColor,
-          body: Padding(
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    // key: ValueKey(1),
-                    // validator: (value) {
-                    //   if (value!.isEmpty) {
-                    //     return 'Please write title!';
-                    //   } else
-                    //     return null;
-                    // },
-                    // onSaved: (value) {
-                    //   _addTitle = value!;
-                    // },
-                    onChanged: (value) {
-                      // _addTitle = value;
-                      setState(() {
-                        _addTitle = value;
-                      });
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Notice title',
-                      hintStyle: TextStyle(fontWeight: FontWeight.bold),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      border: InputBorder.none,
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    // key: ValueKey(2),
-                    // validator: (value) {
-                    //   if (value!.isEmpty) {
-                    //     return 'Please write notice!';
-                    //   } else
-                    //     return null;
-                    // },
-                    // onSaved: (value) {
-                    //   _addText = value!;
-                    // },
-                    onChanged: (value) {
-                      _addText = value;
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Write notice',
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide: BorderSide(color: Colors.blue),
-                      ),
-                      border: InputBorder.none,
-                      filled: true,
-                      fillColor: Colors.white,
-                    ),
-                    maxLines: 20,
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    // crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.end,
+          body: Scaffold(
+            backgroundColor: ColorSet.pageBackgroundColor,
+            body: Padding(
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Column(
                     children: [
-                      GestureDetector(
-                        // onTap: _addText.trim().isEmpty || _addTitle.trim().isEmpty ? null : (){}
-                        onTap:
-                            _addTitle.trim().isEmpty || _addText.trim().isEmpty
-                                ? null
-                                : _sendNotice,
-                        // onTap: _sendNotice,
-                        child: Container(
-                          padding: EdgeInsets.only(
-                              left: 8, right: 8, top: 2, bottom: 2),
-                          height: 30,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            // color: ColorSet.appBarColor),
-                            color: _addTitle.trim().isEmpty ||
-                                    _addText.trim().isEmpty
-                                ? Colors.grey
-                                : ColorSet.appBarColor,
+                      TextField(
+                        textInputAction: TextInputAction.next,
+                        onChanged: (value) {
+                          setState(() {
+                            _addTitle = value;
+                          });
+                        },
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                        decoration: InputDecoration(
+                            hintText: 'Notice title',
+                            hintStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                            border: InputBorder.none,
+                            filled: true,
+                            fillColor: ColorSet.pageBackgroundColor),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 8),
+                        decoration: BoxDecoration(
+                            border:
+                                Border(top: BorderSide(color: Colors.grey))),
+                        child: TextField(
+                          onChanged: (value) {
+                            _addText = value;
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'Write notice',
+                            border: InputBorder.none,
+                            filled: true,
+                            fillColor: ColorSet.pageBackgroundColor,
                           ),
-                          child: Row(
-                            children: <Widget>[
-                              Icon(
-                                Icons.add,
-                                color: Colors.white,
-                                size: 20,
-                              ),
-                              SizedBox(
-                                width: 2,
-                              ),
-                              Text(
-                                'Add Notice',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
+                          maxLines: null,
                         ),
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
---
+++ lib/Notice/testNotice.dart
@@ -0,0 +1,202 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:ui_1/color/shareColor.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AddNotice extends StatefulWidget {
+  const AddNotice({Key? key}) : super(key: key);
+
+  @override
+  State<AddNotice> createState() => _AddNoticeState();
+}
+
+class _AddNoticeState extends State<AddNotice> {
+  var _addTitle = '';
+  var _addText = '';
+
+  void _snackBarMessage() {  //스낵바 활용
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Expanded(
+          child: Text('Done'),
+        ),
+        duration: Duration(
+          seconds: 2,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        behavior: SnackBarBehavior.floating, //바닥과 공간 두는 기능
+      ),
+    );
+  }
+
+  void _sendNotice() async {
+    final user = FirebaseAuth.instance.currentUser;
+    final userData = await FirebaseFirestore.instance
+        .collection('user')
+        .doc(user!.uid)
+        .get();
+    FirebaseFirestore.instance.collection('notice').add({
+      'noticeTitle': _addTitle,
+      'noticeText': _addText,
+      'time': Timestamp.now(),
+      'dateTime': Timestamp.now().toDate().toString(),
+      'userName': userData.data()!['userName'],
+    });
+    Navigator.pop(context);
+    _snackBarMessage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: ColorSet.appBarColor,
+          flexibleSpace: SafeArea(
+            child: Container(
+              padding: EdgeInsets.only(right: 16),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'New Notice',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: Scaffold(
+          backgroundColor: ColorSet.pageBackgroundColor,
+          body: Padding(
+            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    textInputAction: TextInputAction.next,
+                    onChanged: (value) {
+                      setState(() {
+                        _addTitle = value;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Notice title',
+                      hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                      // enabledBorder: OutlineInputBorder(
+                      //   borderRadius: BorderRadius.all(Radius.circular(8)),
+                      //   borderSide: BorderSide(color: Colors.blue),
+                      // ),
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(height: 400,
+                  padding: EdgeInsets.only(top: 8),
+                  decoration: BoxDecoration(color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(8))),
+                  child : TextField(
+                    // textInputAction: TextInputAction.done,
+                    onChanged: (value) {
+                      _addText = value;
+                    },
+                    decoration: InputDecoration(
+                      hintText: 'Write notice',
+                      // enabledBorder: OutlineInputBorder(
+                      //   borderRadius: BorderRadius.all(Radius.circular(8),),
+                      //   borderSide: BorderSide(color: Colors.blue),
+                      // ),
+                      border: InputBorder.none,
+                      filled: true,
+                      fillColor: Colors.white,
+                    ),
+                    maxLines: null,
+                  ),),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap:
+                            _addTitle.trim().isEmpty || _addText.trim().isEmpty // 둘 중 하나라도 비어있으면 실행 안되게
+                                ? null
+                                : _sendNotice,
+                        child: Container(
+                          padding: EdgeInsets.only(
+                              left: 8, right: 8, top: 2, bottom: 2),
+                          height: 30,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: _addTitle.trim().isEmpty ||
+                                    _addText.trim().isEmpty
+                                ? Colors.grey
+                                : ColorSet.appBarColor,
+                          ),
+                          child: Row(
+                            children: <Widget>[
+                              Icon(
+                                Icons.add,
+                                color: Colors.white,
+                                size: 20,
+                              ),
+                              SizedBox(
+                                width: 2,
+                              ),
+                              Text(
+                                'Add Notice',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
