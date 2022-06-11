7e46841ce0545c45a881e8a6a6b27bfa5727257e
Jeon-Ji-yeon <86143602+Jeon-Ji-yeon@users.noreply.github.com>
2022-06-12 04:50:49 +0900

    Add files via upload
    getx 사용 위해 controller생성

--- lib/color/shareColor.dart
+++ lib/color/shareColor.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ColorSet{
   static const pageBackgroundColor = Color(0xffDAE0E8);
   static const appBarColor = Color(0xff5D8AB7);
+}
+
+class noticeColor{
+  noticeColor({this.color1 = Colors.blue, this.color2 = Colors.red});
+
+  Color color1;
+  Color color2;
+
+}
+
+class Controller extends GetxController{
+  var noticecolor = noticeColor().obs;
+
+  void updatecolor(ncolor1, ncolor2){
+    noticecolor.update((val) {
+      val?.color1 = ncolor1;
+      val?.color2 = ncolor2;
+    });
+  }
 }
