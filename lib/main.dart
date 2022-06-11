570ff2766821874504862123f55ef6bde1c213e0
Jeon-Ji-yeon <86143602+Jeon-Ji-yeon@users.noreply.github.com>
2022-06-12 04:51:29 +0900

    Add files via upload
    getx사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_1/screen/login.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,13 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: '개척-Talk',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginForm(),
+    return ScreenUtilInit(
+      designSize: Size(360, 690),
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: '개척-Talk',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: LoginForm(),
+        );
+      },
     );
   }
 }
